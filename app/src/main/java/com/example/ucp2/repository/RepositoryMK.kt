c4f60cc8609e88052af443f3742f6f8cf8076999
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-12-18 19:38:47 +0700

    menambahkan RepositoryMK

--- app/src/main/java/com/example/ucp2/repository/RepositoryMK.kt
+++ app/src/main/java/com/example/ucp2/repository/RepositoryMK.kt
@@ -1,4 +1,16 @@
 package com.example.ucp2.repository
 
+import androidx.room.Update
+import com.example.ucp2.data.entity.Matakuliah
+import kotlinx.coroutines.flow.Flow
+
 interface RepositoryMK {
+    suspend fun insertMK(matakuliah: Matakuliah)
+    fun getAllMatakuliah(): Flow<List<Matakuliah>>
+
+    fun getMK(kode: String): Flow<Matakuliah>
+
+    suspend fun deleteMK(matakuliah: Matakuliah)
+
+    suspend fun UpdateMK(matakuliah: Matakuliah)
 }
