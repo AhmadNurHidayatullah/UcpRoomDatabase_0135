b1f0ef0b671506fe61a6548a47579c7631087b30
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-12-19 15:02:31 +0700

    Menambahkan data class HomeUiState untuk mengelola state UI di HomeView

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/dosen/HomeDosenViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.ucp2.ui.viewmodel.dosen
+
+import com.example.ucp2.data.entity.Dosen
+
+
+data class HomeUiState(
+    val listDosen: List<Dosen> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
+
