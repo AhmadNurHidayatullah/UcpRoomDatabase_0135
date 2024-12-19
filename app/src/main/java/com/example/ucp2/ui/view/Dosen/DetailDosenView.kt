3e68741cc3043a62ac449b1f049c7d5e77f394d8
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-12-19 16:50:24 +0700

    Menambahkan BodyDetailDosen untuk menampilkan data dosen

--- app/src/main/java/com/example/ucp2/ui/view/Dosen/DetailDosenView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/Dosen/DetailDosenView.kt
@@ -1,22 +1,79 @@
 package com.example.appmahasiswa.ui.view.dosen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ucp2.data.entity.Dosen
+import com.example.ucp2.ui.viewmodel.dosen.DetailUiState
+import com.example.ucp2.ui.viewmodel.dosen.DosenUiState
+import com.example.ucp2.ui.viewmodel.dosen.toDosenEntity
 
+
+@Preview(showBackground = true)
+@Composable
+fun BodyDetailDosen(
+    modifier: Modifier = Modifier,
+    DetailDosenUiState: DetailUiState = DetailUiState(),
+) {
+    // state for showing the insert confirmation dialog
+    var insertConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+
+    when {
+        DetailDosenUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator() // Show loading indicator
+            }
+        }
+
+        DetailDosenUiState.isError -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Failed to load data. Please try again.",
+                    color = Color.Red,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+        else -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Data Tidak Ditemukan",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
 
 @Composable
 fun DataDetailDosen(
