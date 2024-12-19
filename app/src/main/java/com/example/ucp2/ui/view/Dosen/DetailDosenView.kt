6c6a7d11852a6253889030537d1f451d5961c2b4
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-12-19 16:31:04 +0700

    Menambahkan itemDetailDosen untuk menampilkan detail dosen

---
+++ app/src/main/java/com/example/ucp2/ui/view/Dosen/DetailDosenView.kt
@@ -0,0 +1,34 @@
+package com.example.appmahasiswa.ui.view.dosen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun itemDetailDosen(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String // Dosen data content
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya, fontSize = 20.sp, // Display the content
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
