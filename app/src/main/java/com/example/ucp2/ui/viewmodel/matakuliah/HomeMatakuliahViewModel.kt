ea237c201e6879179b164e89a66808b36e907be5
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-12-21 01:40:23 +0700

    Menambahkan HomeMatakuliahViewModel untuk mengelola UI state halaman Matakuliah

--- app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/HomeMatakuliahViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/HomeMatakuliahViewModel.kt
@@ -1,10 +1,56 @@
 package com.example.ucp2.ui.viewmodel.matakuliah
 
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2.data.entity.Matakuliah
+import com.example.ucp2.repository.RepositoryMK
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+
+class HomeMatakuliahViewModel(
+    private val repositoryMK: RepositoryMK
+) : ViewModel() {
+
+    val homeUiState: StateFlow<HomeUiState> = repositoryMK.getAllMatakuliah()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMk = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError =  true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
+}
 
 data class HomeUiState(
-    val listDosen: List<Matakuliah> = listOf(),
+    val listMk: List<Matakuliah> = listOf(),
     val isLoading: Boolean = false,
     val isError: Boolean = false,
     val errorMessage: String = ""
