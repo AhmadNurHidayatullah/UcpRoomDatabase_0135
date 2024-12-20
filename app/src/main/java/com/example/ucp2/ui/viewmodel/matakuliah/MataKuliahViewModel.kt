0e71da07c412c826ee6de0550518fdaa7d970f83
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-12-21 01:44:34 +0700

    menambahkan data class variabel yang menyimpan data

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/MataKuliahViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.ucp2.ui.viewmodel.matakuliah
+
+data class MatakuliahEvent(
+    val Kode: String = "",
+    val Nama: String = "",
+    val SKS: String = "",
+    val Semester: String = "",
+    val jenis: String = "",
+    val DosenPengampu: String = ""
+)
