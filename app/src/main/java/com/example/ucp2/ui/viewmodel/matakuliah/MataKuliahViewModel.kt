29b356dd3a53e4e5fec745ed7becfce6a1858268
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-12-21 02:20:40 +0700

    Menambahkan validasi form dan logika penyimpanan untuk MataKuliah

--- app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/MataKuliahViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/MataKuliahViewModel.kt
@@ -1,8 +1,64 @@
 package com.example.ucp2.ui.viewmodel.matakuliah
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2.data.entity.Matakuliah
+import com.example.ucp2.repository.RepositoryMK
+import kotlinx.coroutines.launch
 
+class MataKuliahViewModel(private val repositoryMK: RepositoryMK): ViewModel(){
 
+    var uiState by mutableStateOf(MkUiState())
+
+    fun UpdateState(matakuliahEvent: MatakuliahEvent){
+        uiState = uiState.copy(
+            matakuliahEvent = matakuliahEvent
+        )
+    }
+
+    private fun validateFields(): Boolean{
+        val event = uiState.matakuliahEvent
+        val errorState = FormErrorState(
+            Nama = if (event.Nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            Kode = if (event.Kode.isNotEmpty()) null else "Nama tidak boleh kosong",
+            SKS = if (event.SKS.isNotEmpty()) null else "Nama tidak boleh kosong",
+            Semester = if (event.Semester.isNotEmpty()) null else "Nama tidak boleh kosong",
+            jenis = if (event.jenis.isNotEmpty()) null else "Nama tidak boleh kosong",
+            DosenPengampu = if (event.DosenPengampu.isNotEmpty()) null else "Nama tidak boleh kosong"
+        )
+        uiState = uiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+    fun saveData(){
+        val currentEvent = uiState.matakuliahEvent
+        if (validateFields()){
+            viewModelScope.launch {
+                try {
+                    repositoryMK.insertMK(currentEvent.toMatakuliahEntity())
+                    uiState = uiState.copy(
+                        snackbarMessage = "Data berhasil disimpan",
+                        matakuliahEvent = MatakuliahEvent(),
+                        isEntryValid = FormErrorState()
+                    )
+                } catch (e: Exception){
+                    uiState = uiState.copy(
+                        snackbarMessage = "Data gagal disimpan"
+                    )
+                }
+            }
+        }else{
+            uiState = uiState.copy(
+                snackbarMessage = "Input tidak valid. periksa kembali data anda"
+            )
+        }
+    }
+    fun resetSnackBarMessage(){
+        uiState = uiState.copy(snackbarMessage = null)
+    }
+}
 
 data class MkUiState(
     val matakuliahEvent: MatakuliahEvent = MatakuliahEvent(),
