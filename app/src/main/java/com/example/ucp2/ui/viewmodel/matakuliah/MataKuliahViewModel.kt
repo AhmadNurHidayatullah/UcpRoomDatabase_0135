9e923f2ce098da24348b9f952bbedf1e9d71d9c7
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-12-21 01:51:04 +0700

    Menambahkan fungsi mapping MatakuliahEvent

--- app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/MataKuliahViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/MataKuliahViewModel.kt
@@ -1,5 +1,17 @@
 package com.example.ucp2.ui.viewmodel.matakuliah
 
+import com.example.ucp2.data.entity.Matakuliah
+
+
+fun MatakuliahEvent.toMatakuliahEntity(): Matakuliah = Matakuliah (
+    Kode = Kode,
+    Nama = Nama,
+    SKS = SKS,
+    Semester = Semester,
+    jenis = jenis,
+    DosenPengampu = DosenPengampu
+)
+
 data class MatakuliahEvent(
     val Kode: String = "",
     val Nama: String = "",
