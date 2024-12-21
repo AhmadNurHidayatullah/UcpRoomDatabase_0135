e0eb2540884e9a18f1229621fcfc5c1fe3d65813
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-12-21 15:43:26 +0700

    Menambahkan DetailUiState untuk menyimpan detail Matakuliah

--- app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/DetailMatakuliahViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/DetailMatakuliahViewModel.kt
@@ -2,6 +2,20 @@ package com.example.ucp2.ui.viewmodel.matakuliah
 
 import com.example.ucp2.data.entity.Matakuliah
 
+data class DetailUiState(
+    val detailUiState: MatakuliahEvent = MatakuliahEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiState == MatakuliahEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get()= detailUiState!= MatakuliahEvent()
+}
+
+
 fun Matakuliah.toDetailUiEvent(): MatakuliahEvent{
     return MatakuliahEvent(
         Kode = Kode,
