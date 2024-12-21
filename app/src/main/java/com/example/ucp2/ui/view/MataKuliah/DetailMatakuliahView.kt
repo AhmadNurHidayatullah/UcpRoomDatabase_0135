523e872efae6f02c3fd4c3a5a1c17a2a5914915c
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-12-21 20:00:43 +0700

    Menambahkan DeleteConfirmationDialog untuk konfirmasi penghapusan data

---
+++ app/src/main/java/com/example/ucp2/ui/view/MataKuliah/DetailMatakuliahView.kt
@@ -0,0 +1,31 @@
+package com.example.ucp2.ui.view.MataKuliah
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    AlertDialog(
+        onDismissRequest = {/* Do nothing */},
+        title = { Text("Delete Data") },
+        text = { Text("Apakah anda yakin ingin menghapus data?") },
+        modifier = Modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Yes")
+            }
+        }
+    )
+}
